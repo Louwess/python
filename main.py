3f9fdf3a0fda705a1ff37a4c4a0e7db36963af72
Louwess <louishep@gmail.com>
2021-11-11 01:58:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+import RPi.GPIO as GPIO
+import time
+
+# Pin Definitons:
+ledPin = 18 # Broadcom pin 18
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+
+GPIO.output(ledPin, GPIO.LOW)
+time.sleep(0.5)
+GPIO.output(ledPin, GPIO.HIGH)
+time.sleep(0.5)
