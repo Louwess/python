6840819d159db25720eef2efc9cd4f749985cd7d
Louwess <louishep@gmail.com>
2021-11-11 02:02:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
 
 GPIO.output(ledPin, GPIO.LOW)
+print("low")
 time.sleep(0.5)
 GPIO.output(ledPin, GPIO.HIGH)
+print("high")
 time.sleep(0.5)
